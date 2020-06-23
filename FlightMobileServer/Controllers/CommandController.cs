06c5b59453785f6dc3f0d9388c810c20147ea27a
yehonatansofri <ysofri@gmail.com>
2020-06-23 16:40:35 +0300

    changed folder name

---
+++ FlightMobileServer/Controllers/CommandController.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Controller for posting commands.
+ * 
+ * author: Jonathan Sofri.
+ * date: 15/6/20.
+ */
+using FlightControlAndroid.Models;
+using FlightControlAndroid.Util;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace FlightControlAndroid.Controllers
+{
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class CommandController : ControllerBase
+    {
+        private IModel _model;
+
+        /*
+         * Ctor.
+         */
+        public CommandController(IModel model, IConfiguration config)
+        {
+            var port = config.GetSection("SimulatorTelnetPort").Value;
+            var host = config.GetSection("SimulatorTelnetHost").Value;
+            _model = model;
+            _model.SetTelnetClient(host, port);
+        }
+
+        // POST: api/Command
+        [HttpPost]
+        public async Task<int> Post([FromBody] Command command)
+        {
+            var answer = await _model.PostCommand(command);
+
+            return (int)answer;
+        }
+    }
+}
---
+++ FlightMobileServer/Models/Model.cs
@@ -0,0 +1,71 @@
+﻿/*
+ * Concrete class of modal for excercise.
+ * 
+ * author: Jonathan Sofri.
+ * date: 15/6/20.
+ */
+using FlightControlAndroid.Util;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FlightControlAndroid.Models
+{
+    public class Model : IModel
+    {
+        // different client for each request.
+        private FlightGearClient _simulatorClient = null;
+        private HttpImageClient _imageClient = null;
+
+        /*
+         * Ctor.
+         */
+        public Model() { }
+
+        /*
+         * Get a command object, use FlightGearClient to set data.
+         * return status code (int).
+         */
+        public async Task<int> PostCommand(Command command)
+        {
+            Result res;
+
+            if (_simulatorClient != null)
+            {
+                res = await _simulatorClient.Execute(command);
+            }
+            else
+            {
+                res = Result.ExternalServerError;
+            }
+
+            return (int)res;
+        }
+
+        /*
+         * Use HttpClient to get image and return data.
+         */
+        public async Task<ActionResult> GetScreenshot()
+        {
+            ActionResult actionResult = null;
+
+            if (_imageClient != null)
+            {
+                actionResult = await _imageClient.GetImage();
+            }
+
+            return actionResult;
+        }
+
+        public void SetImageClient(string host, string port)
+        {
+            if (_imageClient == null)
+                _imageClient = new HttpImageClient(host, port);
+        }
+
+        public void SetTelnetClient(string host, string port)
+        {
+            if (_simulatorClient == null)
+                _simulatorClient = new FlightGearClient(host, port);
+        }
+    }
+}
